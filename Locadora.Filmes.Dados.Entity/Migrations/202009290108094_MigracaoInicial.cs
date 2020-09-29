d77b89b58a705b96471f39a6b0cdab318e3375ac
Douglas Silva Oliveira <douglas.silva22@hotmail.com>
2020-09-29 19:06:13 -0300

    first commit

---
+++ Locadora.Filmes.Dados.Entity/Migrations/202009290108094_MigracaoInicial.cs
@@ -0,0 +1,28 @@
+﻿namespace Locadora.Filmes.Dados.Entity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigracaoInicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Albuns",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false, maxLength: 50),
+                        Ano = c.Int(nullable: false),
+                        Descricao = c.String(maxLength: 1000),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Albuns");
+        }
+    }
+}
---
+++ Locadora.Filmes.Dados.Entity/TypeConfiguration/AlbumTypeConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Locadora.Filmes.Dominio;
+using Locadora.Filmes.Generica.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Filmes.Dados.Entity.TypeConfiguration
+{
+    class AlbumTypeConfiguration : LocadoraEntityAbstractConfig<Album>
+    {
+        protected override void ConfigurarCamposTabela()
+        {
+            Property(p => p.Id)
+                .IsRequired()
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)
+                .HasColumnName("Id");
+
+            Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("Nome");
+
+            Property(p => p.Ano)
+                .IsRequired()
+                .HasColumnName("Ano");
+
+            Property(p => p.Descricao)
+                .HasColumnName("Descricao")
+                .HasMaxLength(1000);
+        }
+
+        protected override void ConfigurarChaveEstrangeira()
+        {
+            
+        }
+
+        protected override void ConfigurarChavePrimaria()
+        {
+            HasKey(pk => pk.Id);
+        }
+
+        protected override void ConfigurarNomeTabela()
+        {
+            ToTable("Albuns");
+        }
+    }
+}
